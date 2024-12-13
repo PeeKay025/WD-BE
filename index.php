55c050d3b1f66751461d71485cdd2e1e4da5775d
Prince Karl de Grano <degranoprincekarl@gmail.com>
2024-12-13 08:01:27 +0800

    no content yet

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Island of Personality</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <style>
+        body {
+            background-image: url('images/IoPbg.png');
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="container-fluid">
+        <div class="row">
+            <img src="images/disney-pixar-logo 1.png" alt="" class="dsnypxLogo">
+        </div>
+    </div>
+    
+    <div class="container">
+        <div class="row text-center">
+            <!-- First Column -->
+            <div class="col-md-3 col-sm-6 mb-4">
+                <img src="images/cat.jpg" alt="Cat Image" class="img-fluid rounded">
+            </div>
+
+            <!-- Second Column -->
+            <div class="col-md-3 col-sm-6 mb-4">
+                <img src="images/cat.jpg" alt="Cat Image" class="img-fluid rounded">
+            </div>
+
+            <!-- Third Column -->
+            <div class="col-md-3 col-sm-6 mb-4">
+                <img src="images/cat.jpg" alt="Cat Image" class="img-fluid rounded">
+            </div>
+
+            <!-- Fourth Column -->
+            <div class="col-md-3 col-sm-6 mb-4">
+                <img src="images/cat.jpg" alt="Cat Image" class="img-fluid rounded">
+            </div>
+        </div>
+    </div>
+
+
+    <!-- <div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
+        <div class="w3-display-topleft w3-padding-large w3-xlarge">
+            Logo
+        </div>
+        <div class="w3-display-middle">
+            <h1 class="w3-jumbo w3-animate-top">COMING SOON</h1>
+            <hr class="w3-border-grey" style="margin:auto;width:40%">
+            <p class="w3-large w3-center">35 days left</p>
+        </div>
+    </div> -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
